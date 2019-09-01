70808324b6e62b379c79e60da79675b76037a3dc
dsohenrique <dsohenrique@gmail.com>
2019-08-31 21:07:35 -0300

    Cria projeto.

---
+++ Semana 16/README.md
@@ -0,0 +1,5 @@
+# Testes unitários
+	Inovar mais reduzindo a possibilidade de erros do código.
+	
+# Porque devemos testar o código de forma unitária?
+	Para garantir que o código funciona, sobre várias circunstâncias com diversas entradas, aumentando assim a qualidade do código e melhorando também a manipulação de erros através de baby-steps.
---
+++ Semana 16/TestesUnitarios/MyClasses/FileProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MyClasses
+{
+    class FileProcess
+    {
+        public bool fileExisits(string fileName) {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("FileName");
+            }
+            return File.Exists(fileName);
+        }
+    }
+}

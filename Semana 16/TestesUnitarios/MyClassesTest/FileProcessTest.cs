0eb96ca5163e8338194764b03916ab134794e126
dsohenrique <dsohenrique@gmail.com>
2019-08-31 21:15:35 -0300

    Cria projeto de teste.

---
+++ Semana 16/TestesUnitarios/MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExisit()
+        {
+            //TODO;
+            Assert.Inconclusive();
+        }
+
+        public void FileNameDoesNotExisit()
+        {
+            //TODO;
+            Assert.Inconclusive();
+        }
+
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            //TODO;
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ Semana 16/TestesUnitarios/MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57b152ba-631e-4ff3-aebc-fbba1527fecd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

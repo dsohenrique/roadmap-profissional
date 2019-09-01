d8db0293c487064201dc545c0b59f8900f82a6f0
dsohenrique <dsohenrique@gmail.com>
2019-09-01 13:11:09 -0300

    Adiociona testes Initialize e CleanUp.

--- Semana 16/TestesUnitarios/MyClassesTest/FileProcessTest.cs
+++ Semana 16/TestesUnitarios/MyClassesTest/FileProcessTest.cs
@@ -1,22 +1,54 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
 using System;
+using System.IO;
 
 namespace MyClassesTest
 {
     [TestClass]
     public class FileProcessTest
     {
+        private const string GOOD_FILE_NAME = @"c:\temp\testesUnitarios.txt";
+        public TestContext TestContext { get; set; }
+
+        #region Test Initialize e CleanUp
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (TestContext.TestName == "FileNameDoesExisit")
+            {
+                if (string.IsNullOrEmpty(GOOD_FILE_NAME))
+                {
+                    TestContext.WriteLine($"Creating File {GOOD_FILE_NAME}");
+                    File.AppendAllText(GOOD_FILE_NAME, "Hello World");
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (string.IsNullOrEmpty(GOOD_FILE_NAME))
+            {
+                TestContext.WriteLine($"Deleting File {GOOD_FILE_NAME}");
+                File.Delete(GOOD_FILE_NAME);
+            }
+        }
+
+
         [TestMethod]
         public void FileNameDoesExisit()
         {
             FileProcess fileProcess = new FileProcess();
             bool fromCall;
 
-            fromCall = fileProcess.fileExisits(@"c:\temp\test.txt");
+            TestContext.WriteLine($"Testing File {GOOD_FILE_NAME}");
+            fromCall = fileProcess.fileExisits(GOOD_FILE_NAME);
+
             Assert.IsTrue(fromCall);
         }
         
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_ThrowsArgumentNullException()
@@ -37,7 +69,6 @@ namespace MyClassesTest
             }
             catch (Exception)
             {
-
                 return;
             }
             Assert.Fail("Fail Expected");

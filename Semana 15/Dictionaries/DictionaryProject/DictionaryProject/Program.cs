7e72e3235fb91a13be94f4efe6147e3b7cf29d55
Douglas Oliveira <dsohenrique@gmail.com>
2019-09-14 22:01:43 -0300

    Adiciona projeto de dictionary.

---
+++ Semana 15/Dictionaries/DictionaryProject/DictionaryProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+    class Program
+    {
+        public class myDictionary
+        {
+            public IDictionary<string, int> GetClientes()
+            {
+                IDictionary<string, int> clientes = new Dictionary<string, int>();
+                clientes.Add("Douglas", 22);
+                clientes.Add("Camila", 12);
+                clientes.Add("Ricardo", 15);
+                return clientes;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+
+            var _myDictionary = new myDictionary();
+            var myClientes = _myDictionary.GetClientes();
+
+            foreach (var cliente in myClientes)
+            {
+                Console.WriteLine(cliente);
+                Console.ReadLine();
+            }
+        }
+    }
+}

6f986ce3971503c757a441e1440e5bdbe0f5088f
Douglas Oliveira <dsohenrique@gmail.com>
2019-09-14 22:23:48 -0300

    Atualiza projeto de dictionary.

--- Semana 15/Dictionaries/DictionaryProject/DictionaryProject/Program.cs
+++ Semana 15/Dictionaries/DictionaryProject/DictionaryProject/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DictionaryProject
 {
@@ -10,28 +7,46 @@ namespace DictionaryProject
     {
         public class myDictionary
         {
-            public IDictionary<string, int> GetClientes()
+            public IDictionary<string, string> GetClientes()
             {
-                IDictionary<string, int> clientes = new Dictionary<string, int>();
-                clientes.Add("Douglas", 22);
-                clientes.Add("Camila", 12);
-                clientes.Add("Ricardo", 15);
+                //Criando um Dictionary de chave string e valor string
+                IDictionary<string, string> clientes = new Dictionary<string, string>();
+                clientes.Add("Douglas", "22");
+                clientes.Add("Camila", "12");
+                clientes.Add("Ricardo", "15");
+
                 return clientes;
             }
         }
         
         static void Main(string[] args)
         {
-            
-
             var _myDictionary = new myDictionary();
             var myClientes = _myDictionary.GetClientes();
 
+            Console.WriteLine("Percorrendo todos os itens do Dictionary");
             foreach (var cliente in myClientes)
             {
-                Console.WriteLine(cliente);
-                Console.ReadLine();
+                Console.WriteLine("Chave: " + cliente.Key);
+                Console.WriteLine("Valor: " + cliente.Value);
+                Console.WriteLine();
             }
+
+            Console.WriteLine("Percorrendo todos os itens do Dictionary por chaves");
+            foreach (var cliente in myClientes)
+            {
+                Console.WriteLine("Chave: " + cliente.Key);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Percorrendo todos os itens do Dictionary por valor");
+            foreach (var cliente in myClientes)
+            {
+                Console.WriteLine("Valor: " + cliente.Value);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }

45a3fb8df8e9733e0df463c4cfdaaf958ccb2bbb
dsohenrique <33877797+dsohenrique@users.noreply.github.com>
2019-10-31 19:49:54 -0300

    Pokedex App

---
+++ Semana 25/Flutter/pokedex/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/views/homePage.dart';
+
+void main() => runApp(MaterialApp(
+  title: 'PokeApp',
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
